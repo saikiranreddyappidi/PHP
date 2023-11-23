bb827d2036f35b5f318a88b8c35e984cdb72c50d
Sai Kiran Reddy Appidi <98457635+saikiranreddyappidi@users.noreply.github.com>
2023-11-23 12:04:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const userPasswordEl = document.querySelector("#password");
+const togglePasswordEl = document.querySelector("#togglePassword");
+
+togglePasswordEl.addEventListener("click", function () {
+  if (this.checked === true) {
+    userPasswordEl.setAttribute("type", "text");
+  } else {
+    userPasswordEl.setAttribute("type", "password");
+  }
+});
